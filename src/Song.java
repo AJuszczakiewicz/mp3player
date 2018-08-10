fe8fc083e6e36580ba1f01613297b7cae43bf9ea
AJuszczakiewicz <al.jusz@gmail.com>
2018-08-10 03:05:44 +0200

    Refactor constructor

--- src/Song.java
+++ src/Song.java
@@ -3,6 +3,7 @@ import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 class Song extends Thread {
@@ -13,29 +14,32 @@ class Song extends Thread {
     private String album;
     private String year;
     private String path;
-    private final Player playMp3;
+    private Player playMp3;
 
 
     Song(String filePath) throws UnsupportedTagException, InvalidDataException, IOException, JavaLayerException {
+        setup(filePath);
+    }
+
+    private void setup(String filePath) throws IOException, JavaLayerException, InvalidDataException, UnsupportedTagException {
         FileInputStream fis = new FileInputStream(filePath);
         playMp3 = new Player(fis);
         Mp3File mp3File = new Mp3File(filePath);
+        path = filePath;
         if (mp3File.hasId3v1Tag()) {
             ID3v1 id3v1Tag = mp3File.getId3v1Tag();
-            this.title = id3v1Tag.getTitle();
-            this.artist = id3v1Tag.getArtist();
-            this.album = id3v1Tag.getAlbum();
-            this.year = id3v1Tag.getYear();
-            this.path = filePath;
+            title = id3v1Tag.getTitle();
+            artist = id3v1Tag.getArtist();
+            album = id3v1Tag.getAlbum();
+            year = id3v1Tag.getYear();
         }
 
         if (mp3File.hasId3v2Tag()) {
             ID3v2 id3v2Tag = mp3File.getId3v2Tag();
-            this.title = id3v2Tag.getTitle();
-            this.artist = id3v2Tag.getArtist();
-            this.album = id3v2Tag.getAlbum();
-            this.year = id3v2Tag.getYear();
-            this.path = filePath;
+            title = id3v2Tag.getTitle();
+            artist = id3v2Tag.getArtist();
+            album = id3v2Tag.getAlbum();
+            year = id3v2Tag.getYear();
         }
     }
 
